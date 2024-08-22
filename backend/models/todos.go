e41a8da8ca8637bdba1d2d6e93bb8f66e3c1ec3d
Leorio Nguyen <thanhbinhbk888@gmail.com>
2024-08-22 23:38:05 +0700

    update models

--- backend/models/todos.go
+++ backend/models/todos.go
@@ -3,7 +3,7 @@ package models
 import "go.mongodb.org/mongo-driver/bson/primitive"
 
 type Todos struct {
-	ID        primitive.ObjectID
-	Title     string
-	Completed bool
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id,omitempty"`
+	Title     string `bson:"title,omitempty" json:"title,omitempty"`
+	Completed bool `bson:"completed,omitempty" json:"completed,omitempty"`
 }
